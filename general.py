92caa946a7019afeb36caa692d45d4c7d5b59194
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-10 04:45:52 +0530

    Queue and Crawled Files

--- general.py
+++ general.py
@@ -6,3 +6,20 @@ def create_project_dir(directory):
     if not os.path.exists(directory):
         print('creating project '+directory)
         os.makedirs(directory)
+
+# create queue and crawled files (if not created)
+def create_data_files(project_name,base_url):
+    queue = project_name + 'queue.txt'
+    crawled = project_name +'crawled.txt'
+
+    if not os.path.isfile(queue):
+        write_file(queue,base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled,'')
+
+
+# Creates a new file
+def write_file(path,data):
+    f = open(path,'w')
+    f.write(data)
+    f.close()
