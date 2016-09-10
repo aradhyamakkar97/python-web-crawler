471dd2aef15d9a68ca14e5e6e675e3764081ef21
Aradhya Makkar <aradhyamakkar97@gmail.com>
2016-09-10 06:33:23 +0530

    Adding and Deleting Links

--- general.py
+++ general.py
@@ -23,3 +23,13 @@ def write_file(path,data):
     f = open(path,'w')
     f.write(data)
     f.close()
+
+# Add data onto an existing file
+def append_to_file(path,data):
+    with open(path,'a') as file:
+        file.write(data+'\n')
+
+# Delete the contents of a file
+def delete_file_content(path):
+    with open(path,'w'):
+        pass # i.e do nothing
